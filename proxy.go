2c4a286f508db0618b226a1fe6e42002811af52f
Atreya <atreya2011@gmail.com>
2019-06-06 08:41:32 +0900

    first commit

---
+++ proxy.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+
+	"gitlab.com/atreya2011/grpc-practice/postgres-crud/postgrescrud"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+)
+
+// end point means the ip and port on which grpc server is listening
+var postgrescrudEndPoint = flag.String("endpoint", "localhost:7000", "endpoint of PostgresCrudService")
+
+func run() error {
+	// Step 1. define a context with cancel
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Step 2. define the serve mux options here
+	muxOpts := []runtime.ServeMuxOption{
+		// Todo: check meaning of MIMEWildcard
+		// OrigName set to false, json field names will be camelCase
+		// EmitDefaults set to true, empty json fields will not be omitted
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{OrigName: false, EmitDefaults: true}),
+	}
+
+	// Step 3. create a new server mux
+	mux := runtime.NewServeMux(muxOpts...)
+	// Step 4. create grpc dial options for connecting to grpc server
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	// Step 5. register the handler from endpoint (function in generated pb.gw.go file)
+	// the following are passed into the function
+	// context, new server mux, endpoint string, dial options
+	err := postgrescrud.RegisterPostgresCrudHandlerFromEndpoint(ctx, mux, *postgrescrudEndPoint, opts)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Listening...")
+	// Step 6. Start API Server with ListenAndServe, pass in the serve mux
+	// We launch a goroutine for handling requests
+	done := make(chan struct{})
+	go func() {
+		http.ListenAndServe(":8082", mux)
+		done <- struct{}{}
+	}()
+
+	<-done //wait finish groutine
+	log.Println("Program exit")
+	return nil
+}
+
+func main() {
+	// flag.Parse parses the flags passed in the command line
+	// if no flag is present, default values are used
+	// if there is no flag.Parse, command line flags are not parsed
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatalln(err)
+	}
+}
---
+++ server.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"gitlab.com/atreya2011/grpc-practice/postgres-crud/postgrescrud"
+	"google.golang.org/grpc"
+)
+
+type service struct {
+	DB *gorm.DB
+}
+
+var grpcAddrFlag = flag.String("addr", ":7000", "Address host:port")
+
+func main() {
+	log.Printf("grpc server start on port %v", *grpcAddrFlag)
+	// Step 1. listen for connections on tcp
+	lis, err := net.Listen("tcp", *grpcAddrFlag)
+	// Always handle errors
+	if err != nil {
+		log.Fatalf("frak")
+	}
+	// Step 2. Create a new grpc server instance
+	srv := grpc.NewServer()
+	// Step 3. Initialize the db and store it in service{}
+	serv := initDB()
+	// Step 4. Register the Service by passing the new server instance
+	// and the server struct created above
+	// The register function is present in pb.go
+	postgrescrud.RegisterPostgresCrudServer(srv, serv)
+	// Step 5. Serve the listener created above
+	srv.Serve(lis)
+}
+
+// Create The following is the implementation of the Create service
+// as defined in the proto file. It can be any implemention.
+func (s *service) Create(ctx context.Context, req *postgrescrud.CreatePersonRequest) (*postgrescrud.CreatePersonResponse, error) {
+	// create new record by passing req.Person into gorm.Create()
+	log.Printf("%T\n", req.Person.MiddleName)
+	// Note the use of tx as the database handle once you are within a transaction
+	// transactions are used to rollback changes in case of an error
+	// this is to prevent creation of new record in case of an error
+	tx := s.DB.Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	person := &postgrescrud.Person{
+		MiddleName: "",
+	}
+	foo := tx.Model(&postgrescrud.Person{}).Create(person)
+	bar := foo.Error
+	log.Println(bar)
+	if bar != nil {
+		log.Println("Error: ", bar)
+		log.Println("Rollback: ", tx.Rollback().Error)
+		return &postgrescrud.CreatePersonResponse{Id: 0}, bar
+	}
+
+	// return response
+	return &postgrescrud.CreatePersonResponse{Id: 1}, tx.Commit().Error
+}
+
+// Read The following is the implementation of the Read service
+// as defined in the proto file. It can be any implemention.
+func (s *service) Read(ctx context.Context, req *postgrescrud.ReadPersonRequest) (*postgrescrud.ReadPersonResponse, error) {
+	// find person with id
+	// initialize p with a point to person type
+	p := new(postgrescrud.Person)
+	// store the result in p using gorm.Where
+	err := s.DB.Where("id = ?", req.GetId()).First(p).Error
+	if err != nil {
+		log.Println(err)
+	}
+	// return response
+	return &postgrescrud.ReadPersonResponse{Person: p}, nil
+}
+
+// List lists all the fullnames. Request is empty.
+// Below is an example implementation
+func (s *service) List(ctx context.Context, e *empty.Empty) (*postgrescrud.ListPeopleResponse, error) {
+	// initialize slice of pointers to person
+	var people []*postgrescrud.Person
+	// pass this to gorm.Find() to fill it with results
+	err := s.DB.Find(&people).Error
+	if err != nil {
+		log.Println(err)
+	}
+	return &postgrescrud.ListPeopleResponse{Peoples: people}, nil
+}
+
+// Delete deletes record based on id
+// below is an example implementation
+// func (*service) Delete() {
+// 	// Delete - delete product
+// 	db.Delete(&product)
+// }
+
+func initDB() *service {
+	const (
+		host   = "localhost"
+		port   = 5432
+		user   = "atreya"
+		dbname = "atreya"
+	)
+	// Step 1. Build the parameter string based on const declared above
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+	// Step 2. Connect to database with above parameters
+	log.Println(psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Step 3. Migrate the schema
+	db.DropTableIfExists(&postgrescrud.Person{})
+	db.AutoMigrate(&postgrescrud.Person{})
+
+	// Step 4. Create a new service instance and store the db
+	s := &service{db}
+
+	// return the db
+	return s
+}
